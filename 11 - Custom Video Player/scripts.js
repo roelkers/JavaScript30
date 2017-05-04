752a99cb512d38be6514b3421648d3f9ef171dfc
roelkers <rufus.oelkers@gmail.com>
2017-05-04 19:19:23 +0200

    11- Custom HTML Video Player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,7 +4,9 @@ const inputVolume = document.querySelector('input[name="volume"]');
 const inputSpeed = document.querySelector('input[name="playbackRate"]');
 const buttonPlay = document.querySelector('button.toggle');
 const buttonsSkip = document.querySelectorAll('[data-skip]');
-
+const progressBar = document.querySelector('div.progress');
+const progressFilledBar = document.querySelector('div.progress__filled');
+let mousedown;
 /*Build the function */
 function playOrPause(){
   video.paused ?
@@ -20,14 +22,28 @@ function adjustSpeed(event){
   video.playbackRate = event.target.value;
 }
 
+function adjustProgressBar(event){
+  const progressInPct = (video.currentTime/video.duration)*100;
+  progressFilledBar.style.flexBasis = `${progressInPct}%`;
+}
+
 function skip(event){
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progressBar.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 /*Hook up Event Listeners */
 
+video.addEventListener("timeupdate",adjustProgressBar);
 video.addEventListener("click",playOrPause);
 buttonPlay.addEventListener("click", playOrPause);
 inputVolume.addEventListener("change",adjustVolume);
 inputSpeed.addEventListener("change",adjustSpeed);
-buttonsSkip.forEach(button => button.addEventListener("click",skip));
+progressBar.addEventListener("click", scrub);
+progressBar.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progressBar.addEventListener("mousedown", () => mousedown = true);
+progressBar.addEventListener("mouseup", () => mousedown = false);
